7747b25f6deeff4f2dd9f42f1e5fb5089bf4083d
Merlyn Albery-Speyer <merlyn@newrelic.com>
2012-08-20 14:24:48 -0700

    Making xmlsec-ruby build on OSX

--- ext/xmlsec/extconf.rb
+++ ext/xmlsec/extconf.rb
@@ -1,12 +1,10 @@
 require 'mkmf'
-if pkg_config('xmlsec1-openssl') || pkg_config('libxmlsec1')
+if pkg_config('xmlsec1-openssl')
 	# The rightscale image appears to have incorrect
 	# pkg-config files - this flag is needed to link against an 
 	# 64-bit libxmlsec1 but isn't included in the pkg-config 
 	if `uname -p`.match 'x86_64'
 		$CFLAGS += " -DXMLSEC_NO_SIZE_T"
 	end
-	create_makefile('xmlsec')
-else
-	puts "xmlsec1 is not installed."
 end
+create_makefile('xmlsec')
